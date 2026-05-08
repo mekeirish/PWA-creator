0ef3ae152373d941544832e7e74f1e28c5bf6cc0
mekeirish <mmm1747lb@gmail.com>
2026-05-08 13:57:33 +0200

    Refactor service worker caching mechanism
    Updated service worker to use new cache name and improved caching logic.

--- sw.js
+++ sw.js
@@ -1,8 +1,25 @@
-const CACHE_NAME = 'pwa-creator--v1';
-const assets = ['./', './index.html', './manifest.json', './icon.png'];
-self.addEventListener('install', e => {
-    e.waitUntil(caches.open(CACHE_NAME).then(cache => cache.addAll(assets)));
+const CACHE_NAME = 'pwa-studio-v1';
+const assetsToCache = [
+    './',
+    './index.html',
+    './manifest.json',
+    './icon.png'
+];
+
+// Installation : Mise en cache des fichiers
+self.addEventListener('install', (event) => {
+    event.waitUntil(
+        caches.open(CACHE_NAME).then((cache) => {
+            return cache.addAll(assetsToCache);
+        })
+    );
 });
-self.addEventListener('fetch', e => {
-    e.respondWith(caches.match(e.request).then(res => res || fetch(e.request)));
+
+// Fetch : Servir les fichiers depuis le cache si hors-ligne
+self.addEventListener('fetch', (event) => {
+    event.respondWith(
+        caches.match(event.request).then((response) => {
+            return response || fetch(event.request);
+        })
+    );
 });
